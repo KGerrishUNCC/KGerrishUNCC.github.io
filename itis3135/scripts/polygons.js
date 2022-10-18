539a64ac3f85b641c38f8a7b8463afd44b8a59d3
KGerrishUNCC <111810589+KGerrishUNCC@users.noreply.github.com>
2022-10-17 22:23:20 -0400

    Create polygons.js

---
+++ itis3135/scripts/polygons.js
@@ -0,0 +1,44 @@
+function alertUser()
+{
+    var num = prompt("The Keen Gorilla would like your choice of number between 1-11 to find the corresponding polygon");
+    validateEntry(num);
+}
+function validateEntry(_num)
+{
+    if(isNaN(_num)){
+        alert("Sorry! Invalid number entry. Please type between 1-11!");
+        alertUser();
+    }
+    else
+    {
+        number = Math.trunc(_num);
+        if(number > 11 || number < 1)
+        {
+            alert("Sorry! Invalid number entry. Please type between 1-11!");
+            alertUser();
+        }
+        else
+        {
+            getShape(number);
+        }
+    }
+}
+function getShape(sides)
+{
+    var polygonList = new Array(
+        "Monogon", 
+        "Bigon", 
+        "Triangle", 
+        "Quadrilateral", 
+        "Pentagon", 
+        "Hexagon", 
+        "Heptagon", 
+        "Octagon",
+        "Nonagon",
+        "Decagon",
+        "Undecagon"  
+    );
+    var message = "A polygon with " + sides + " sides is called a " + polygonList[sides - 1] + ".";
+
+    document.getElementById("polyName").innerHTML = message;
+}
